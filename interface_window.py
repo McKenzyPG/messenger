a4cb93c14724513e08f5c53710972615c03caab3
Pavel <35500257+McKenzyPG@users.noreply.github.com>
2018-06-09 16:06:47 +0300

    Extended interface opts (file_open, smiles, pics)

--- interface_window.py
+++ interface_window.py
@@ -1,6 +1,10 @@
 import sys
-
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QTextEdit, QLineEdit, QVBoxLayout
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, QLabel,
+    QAction, QFileDialog, QApplication, QLineEdit, QVBoxLayout, QWidget)
+from PyQt5.QtGui import QPixmap, QIcon, QFont
+import random
+from PIL import Image, ImageDraw
+from PIL.ImageQt import ImageQt
 
 
 class TextDisplay(QTextEdit):
@@ -42,11 +46,219 @@ class AppWindow(QMainWindow):
         self.setCentralWidget(widget)
 
 
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+
+	#File upload method
+        self.lbl = QLabel(self)
+
+
+        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open file')
+        openFile.triggered.connect(self.showDialog)
+
+	
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+        fileMenu.addAction(openFile)
+	
+	#image correction method
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('File dialog')
+        self.show()
+
+	image = Image.open("image.jpg")
+        draw = ImageDraw.Draw(image)
+        width = image.size[0]
+        height = image.size[1]
+        pix = image.load()
+
+
+        for i in range(width):
+            for j in range(height):
+
+                 a = pix[i, j][0]
+
+                 b = pix[i, j][1]
+
+                 c = pix[i, j][2]
+
+                 S = (a + b + c) // 2
+
+                 draw.point((i, j), (S, S, S))
+
+
+        img_tmp = ImageQt(image.convert('RGBA'))
+
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap.fromImage(img_tmp)
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+
+        self.move(300, 200)
+        self.setWindowTitle('Example')
+        self.show()
+
+	#font options
+	
+	self.textEdit = QTextEdit()
+        
+        self.setCentralWidget(self.textEdit)
+
+
+        bold = QAction(QIcon('b.jpg'),'Bold', self)
+
+        bold.triggered.connect(self.actionBold)
+
+        italic = QAction(QIcon('i.jpg'), 'Italic', self)
+
+        italic.triggered.connect(self.actionItalic)
+
+        underlined = QAction(QIcon('u.jpg'), 'Underlined', self)
+
+        underlined.triggered.connect(self.actionUnderlined)
+
+
+        toolbar = self.addToolBar('Formatting')
+
+        toolbar.addAction(bold)
+
+        toolbar.addAction(italic)
+
+        toolbar.addAction(underlined)
+
+
+        self.setGeometry(300, 300, 350, 250)
+
+        self.setWindowTitle('Main window')
+
+        self.show()
+
+
+    def actionBold(self):
+
+        myFont = QFont()
+
+        myFont.setBold(True)
+
+        # myFont.setWeight(700)
+
+        self.textEdit.setFont(myFont)
+
+
+    def actionItalic(self):
+
+        myFont = QFont()
+
+        myFont.setItalic(True)
+
+        self.textEdit.setFont(myFont)
+
+
+    def actionUnderlined(self):
+
+        myFont = QFont()
+
+        myFont.setUnderline(True)
+
+        self.textEdit.setFont(myFont)
+
+	#smiles
+
+	self.textEdit = QTextEdit()
+
+        self.setCentralWidget(self.textEdit)
+
+
+        smile = QAction(QIcon('ab.gif'),'Smile', self)
+
+        smile.triggered.connect(self.actionSmile)
+
+        melancholy = QAction(QIcon('ac.gif'), 'Melancholy', self)
+
+        melancholy.triggered.connect(self.actionMelancholy)
+
+        surprise = QAction(QIcon('ai.gif'), 'Surprise', self)
+
+        surprise.triggered.connect(self.actionSurprise)
+
+
+        send = QAction('Send', self)
+
+        send.triggered.connect(self.send_text)
+
+
+        toolbar = self.addToolBar('Formatting')
+
+        toolbar.addAction(smile)
+
+        toolbar.addAction(melancholy)
+
+        toolbar.addAction(surprise)
+
+
+        toolbar.addAction(send)
+
+
+
+        self.setGeometry(300, 300, 350, 250)
+
+        self.setWindowTitle('Main window')
+
+        self.show()
+
+
+    def actionSmile(self):
+
+        url = 'ab.gif'
+
+        self.textEdit.setHtml('<img src="%s" />' % url)
+
+
+    def actionMelancholy(self):
+
+        url = 'ac.gif'
+
+        self.textEdit.setHtml('<img src="%s" />' % url)
+
+    def actionSurprise(self):
+
+        url = 'ai.gif'
+
+        self.textEdit.setHtml('<img src="%s" />' % url)
+
+
+    def send_text(self):
+
+        data = self.textEdit.toHtml()
+
+        print(data.encode())
+
+    def showDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Close file', '/home')[0]
+        pixmap = QPixmap(fname)
+        self.lbl.resize(300,300)
+        self.lbl.setPixmap(pixmap)
+
+       # lbl.setPixmap(pixmap)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    window = AppWindow()
-
-    window.show()
-
+    ex = Example()
     sys.exit(app.exec_())
+
